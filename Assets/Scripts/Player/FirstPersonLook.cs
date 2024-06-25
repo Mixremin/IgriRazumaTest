630326309e1028d45580577664cdcc984cd62a7b
Mixremin <omega8428@gmail.com>
2024-06-25 19:13:22 +0300

    Project commit

---
+++ Assets/Scripts/Player/FirstPersonLook.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    internal class FirstPersonLook : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform character;
+
+        [SerializeField]
+        private float sensitivity = 2;
+
+        [SerializeField]
+        private float smoothing = 1.5f;
+
+        private Vector2 velocity;
+        private Vector2 frameVelocity;
+
+        private void Reset()
+        {
+            character = GetComponentInParent<FirstPersonMovement>().transform;
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            Vector2 mouseDelta = new(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+            frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+            velocity += frameVelocity;
+            velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/FirstPersonMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+namespace Player
+{
+    internal class FirstPersonMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 5;
+
+        [Header("Running")]
+        [SerializeField]
+        private bool canRun = true;
+
+        [SerializeField]
+        private float runSpeed = 9;
+
+        [SerializeField]
+        private KeyCode runningKey = KeyCode.LeftShift;
+
+        private new Rigidbody rigidbody;
+        public bool IsRunning { get; private set; }
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            IsRunning = canRun && Input.GetKey(runningKey);
+
+            float targetMovingSpeed = IsRunning ? runSpeed : speed;
+
+            Vector2 targetVelocity = new(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+            rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+        }
+    }
+}
